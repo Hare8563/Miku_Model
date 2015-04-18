a6dd59e2a1491b2f6b4684dd5969cc8c7e8da686
Hare8563 <haresaku1024@gmail.com>
2015-04-18 18:10:54 +0900

    Pythonを使った自動コミットスクリプトを作成

---
+++ AutoCommit.py
@@ -0,0 +1,40 @@
+#coding: UTF-8
+import sys
+import datetime
+import os
+import time
+
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+
+def getext(filename):
+    return os.path.splitext(filename)[-1].lower()
+
+class ChangeHandler(FileSystemEventHandler):
+	def on_created(self, event):
+		print '"'+ event.src_path + '"が作成されました。Addしてからコミットします'
+		os.system('git commit -am "'+event.src_path+'を作成"')
+	def on_modified(self, event):
+		print '"'+ event.src_path + '"が変更されました。'
+		os.system('git commit -m "'+event.src_path+'を変更"')
+	def on_deleted(self, event):
+		print '"'+ event.src_path + '"が削除されました。'
+		os.system('git commit -m "'+event.src_path+'を削除"')
+
+argvs = sys.argv
+argc = len(argvs)
+if(argc < 2):
+	print "This Script use 1 Argument. Please input checker argc = "+str(argc)
+	quit()
+
+while 1:
+	event_handler = ChangeHandler()
+	observer = Observer()
+	observer.schedule(event_handler, os.path.abspath(argvs[1]), recursive=False)
+	observer.start()
+	try:
+		while True:
+			time.sleep(1)
+	except KeyboardInterrupt:
+		observer.stop()
+	observer.join()
